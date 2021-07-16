71c9ad28ada96fb2a35ca415e997d63514fee8d7
Marv <mrvncaragay@gmail.com>
2021-07-16 13:30:40 -0700

    Ex. 01.js -- Context Module Function

---
+++ src/patterns/ContextModuleFunctions/CounterContext.js
@@ -0,0 +1,52 @@
+import * as React from 'react'
+
+/*
+	Ex. 01.js
+
+	- Passing raw dispatch down also makes code splitting such "async methods" easier. 
+	You import them from the leaf component that uses them instead of the root component. 
+	So you only pay for what you use where you use it. - Dan Abramov
+
+	- Reduce duplication
+	- Helps you avoid mistakes in dependency lists
+
+*/
+
+const CounterContext = React.createContext()
+
+function CounterProvider({step = 1, initialCount = 0, ...props}) {
+  const [state, dispatch] = React.useReducer(
+    (state, action) => {
+      const change = action.step ?? step
+      switch (action.type) {
+        case 'increment': {
+          return {...state, count: state.count + change}
+        }
+        case 'decrement': {
+          return {...state, count: state.count - change}
+        }
+        default: {
+          throw new Error(`Unhandled action type: ${action.type}`)
+        }
+      }
+    },
+    {count: initialCount},
+  )
+
+  const value = [state, dispatch]
+
+  return <CounterContext.Provider value={value} {...props} />
+}
+
+function useCounter() {
+  const context = React.useContext(CounterContext)
+  if (context === undefined) {
+    throw new Error(`useCounter must be used within a CounterProvider`)
+  }
+  return context
+}
+
+const increment = dispatch => dispatch({type: 'increment'})
+const decrement = dispatch => dispatch({type: 'decrement'})
+
+export {CounterProvider, useCounter, increment, decrement}
